d2da0d120db7778f9bee2c465368b9395999abc8
Peter Senyszyn <petergsenyszyn@gmail.com>
2017-11-14 13:41:24 -0600

    First pass through UI and implementing core structures
    Initial UI and class structure for running analysis and viewing results.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,50 @@
-package sample;
+package sample ;
 
-public class Controller {
+import javafx.fxml.FXML ;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller
+{
+    private Parent _root = null ;
+    private Stage _currentStage = null ;
+
+    @FXML
+    private Button _runSentimentAnalysis ;
+
+    @FXML
+    private Button _onViewAnalysisResults ;
+
+    public Controller()
+    {
+
+    }
+
+    @FXML
+    protected void onRunSentimentAnalysisPressed() throws IOException
+    {
+        _root = FXMLLoader.load( getClass().getResource("run.fxml" ) ) ;
+
+        _currentStage = ( Stage )_runSentimentAnalysis.getScene().getWindow() ;
+
+        _currentStage.setScene( new Scene( _root, 1024/2, 768/2 ) ) ;
+        _currentStage.show() ;
+    }
+
+    @FXML
+    protected void onViewAnalysisResultsPressed() throws IOException
+    {
+        _root = FXMLLoader.load( getClass().getResource( "view.fxml" ) ) ;
+
+        _currentStage = ( Stage )_onViewAnalysisResults.getScene().getWindow() ;
+
+        _currentStage.setScene( new Scene ( _root, 1024/2, 768/2 ) ) ;
+        _currentStage.show() ;
+    }
 }
---
+++ src/sample/Run.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class Run
+{
+    private Map<String, Double> _dictionary ;
+
+    @FXML
+    TextArea _textArea ;
+
+    public Run()
+    {
+
+    }
+
+    public void runAnalysis( String path ) throws IOException
+    {
+        
+    }
+}
